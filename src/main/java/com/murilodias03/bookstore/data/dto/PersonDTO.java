1f402ef46500209a86f69dab404ee30b8612d51a
Murilo <murilocristovaodias@gmail.com>
2026-01-08 00:35:17 -0300

    Custom JSON Serialization estudado

--- src/main/java/com/murilodias03/bookstore/data/dto/PersonDTO.java
+++ src/main/java/com/murilodias03/bookstore/data/dto/PersonDTO.java
@@ -1,15 +1,26 @@
 package com.murilodias03.bookstore.data.dto;
 
-import java.io.Serializable;
+import com.fasterxml.jackson.annotation.*;
 
+import java.io.Serializable;
+import java.util.Objects;
+
+@JsonPropertyOrder({"id", "first_name", "last_name", "birth_day", "phone_number", "address", "gender"})
 public class PersonDTO implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     private Long id;
+
+    @JsonProperty("first_name")
     private String firstName;
+
+    @JsonProperty("last_name")
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String lastName;
+
     private String address;
+
     private String gender;
 
     public PersonDTO() {
@@ -53,5 +64,17 @@ public class PersonDTO implements Serializable {
 
     public void setGender(String gender) {
         this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        PersonDTO personDTO = (PersonDTO) o;
+        return Objects.equals(getId(), personDTO.getId()) && Objects.equals(getFirstName(), personDTO.getFirstName()) && Objects.equals(getLastName(), personDTO.getLastName()) && Objects.equals(getAddress(), personDTO.getAddress()) && Objects.equals(getGender(), personDTO.getGender());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getFirstName(), getLastName(), getAddress(), getGender());
     }
 }
