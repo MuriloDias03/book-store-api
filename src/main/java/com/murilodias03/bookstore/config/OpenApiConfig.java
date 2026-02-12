01ba8f992e7db6bf1e6aadc6e9e3eeecf5664739
Murilo <murilocristovaodias@gmail.com>
2026-02-12 12:40:47 -0300

    Projeto preparado para implantar continuous deploymente com GKE V1

--- src/main/java/com/murilodias03/bookstore/config/OpenApiConfig.java
+++ src/main/java/com/murilodias03/bookstore/config/OpenApiConfig.java
@@ -1,5 +1,6 @@
 package com.murilodias03.bookstore.config;
 
+import com.murilodias03.bookstore.services.InstanceInformationService;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
@@ -9,13 +10,19 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class OpenApiConfig {
 
+    private final InstanceInformationService instanceInformationService;
+
+    public OpenApiConfig(InstanceInformationService instanceInformationService) {
+        this.instanceInformationService = instanceInformationService;
+    }
+
     @Bean
     OpenAPI customOpenApi() {
         return new OpenAPI()
                 .info(new Info()
                         .title("Book Store")
                         .version("v1")
-                        .description("REST API's RESTful from 0 with Java, Spring Boot, Kubernetes and Docker - Foo Bar")
+                        .description("REST API's RESTful from 0 with Java, Spring Boot, Kubernetes and Docker V1 " + instanceInformationService.retrieveInstanceInfo())
                         .termsOfService("https://github.com/MuriloDias03")
                         .license(new License()
                                 .name("MIT License")
